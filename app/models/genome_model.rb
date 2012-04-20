bdcd9682c6d00d229fe69c70ea1207d1bca54311
Anthony Brummett <abrummet@genome.wustl.edu>
2012-04-20 15:59:22 -0500

    Added section for model-links to the model page

--- app/models/genome_model.rb
+++ app/models/genome_model.rb
@@ -6,6 +6,11 @@ class GenomeModel < ActiveRecord::Base
   has_many :processing_profile_params, through: :processing_profile
   has_many :model_inputs, :foreign_key => 'model_id'
   belongs_to :subject
+  has_many :from_model_links, :class_name => 'ModelLink', foreign_key: 'from_model_id'
+  has_many :to_model_links, :class_name => 'ModelLink', foreign_key: 'to_model_id'
+#  has_many :child_linked_models, class_name: 'genome_model', foreign_key: 'from_model_id', join_table: 'model_link'
+#  has_many :parent_linked_models, class_name: 'genome_model', foreign_key: 'to_model_id', join_table: 'model_link'
+
 end
 
 
