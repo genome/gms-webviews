7d866547236ba374a60dc1fd89579e8706b664ef
Adam Coffman <acoffman@genome.wustl.edu>
2012-04-20 14:05:16 -0500

    fixes to account for genome_model rename

--- app/models/model_group.rb
+++ app/models/model_group.rb
@@ -1,6 +1,6 @@
 class ModelGroup < ActiveRecord::Base
   set_table_name "model_group"
-  has_and_belongs_to_many :genome_models, join_table: 'model_group_bridge'
+  has_and_belongs_to_many :genome_models, association_foreign_key: 'model_id', join_table: 'model_group_bridge'
 
   def self.model_group_view(id)
     where(id: id).includes(:genome_models => [:processing_profile, :processing_profile_params, :subject, {:builds => [:event]}, :model_inputs]).first
