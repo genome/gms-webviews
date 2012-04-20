19bd60eeeecc6d98150df43da6f6f6de2c089582
Anthony Brummett <abrummet@genome.wustl.edu>
2012-04-20 14:54:34 -0500

    Fix foreign key column name with genome_model

--- app/models/genome_model.rb
+++ app/models/genome_model.rb
@@ -1,10 +1,10 @@
 class GenomeModel < ActiveRecord::Base
   self.table_name = "model"
   has_and_belongs_to_many :model_groups, join_table: 'model_group_bridge'
-  has_many :builds
+  has_many :builds, :foreign_key => 'model_id'
   belongs_to  :processing_profile
   has_many :processing_profile_params, through: :processing_profile
-  has_many :model_inputs
+  has_many :model_inputs, :foreign_key => 'model_id'
   belongs_to :subject
 end
 
