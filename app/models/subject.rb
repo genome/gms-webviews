fd07eb6ae1bc7c9a4159508e0b57a326fe8bb622
Adam Coffman <acoffman@genome.wustl.edu>
2012-04-23 16:31:53 -0500

    subject attribute, sample view

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -2,12 +2,19 @@ class Subject < ActiveRecord::Base
   self.table_name = "subject"
   has_many :genome_models
   has_many :subject_attributes
-
-  def taxon
-    Subject.where(subject_id: self.subject_attributes.where(attribute_label: :taxon_id).first.attribute_value).first
-  end
+  has_many :libraries, foreign_key: 'sample_id'
 
   scope :with_models_and_builds, includes(genome_models: [{builds: :master_event}, :model_inputs])
-  #scope :with_taxon, includes(:taxon)
-  #scope :with_library, includes(:library)
+  scope :with_attributes, includes(:subject_attributes)
+  scope :with_libraries, includes(:libraries)
+
+  def taxon
+    taxon_id = self.subject_attributes.select{|a| a.attribute_label == 'taxon_id'}.first
+    if taxon_id
+      Subject.where(subject_id: taxon_id.attribute_value).first if taxon_id
+    else
+      nil
+    end
+  end
+
 end
