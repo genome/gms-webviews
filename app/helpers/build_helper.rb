8b08e01e7a54ce25d416c573b67d3d80a5b94977
Anthony Brummett <abrummet@genome.wustl.edu>
2012-04-24 10:27:21 -0500

    Get the build status rows to render even if there's no master event for that build

--- app/helpers/build_helper.rb
+++ app/helpers/build_helper.rb
@@ -3,14 +3,18 @@ module BuildHelper
     # status of a build
     def status_label_for_build(build)
         classes = ['label']
-        status = build.master_event.event_status
-        case status
-        when 'Succeeded'
-            classes.push('label-success')
-        when 'Failed'
-            classes.push('label-important')
-        when 'Running'
-            classes.push('label-info');
+        if (build.master_event)
+            status = build.master_event.event_status
+            case status
+            when 'Succeeded'
+                classes.push('label-success')
+            when 'Failed'
+                classes.push('label-important')
+            when 'Running'
+                classes.push('label-info');
+            end
+        else
+            status = 'No events'
         end
         content_tag(:span, status, :class => classes)
     end
