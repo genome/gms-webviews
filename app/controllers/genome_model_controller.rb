7aea948cd4cbf559efc2bb0c787bd88df397634e
Adam Coffman <acoffman@genome.wustl.edu>
2012-04-24 13:44:38 -0500

    model view cleaned up, input helper added, reduce sql queries for model inputs to 1

--- app/controllers/genome_model_controller.rb
+++ app/controllers/genome_model_controller.rb
@@ -3,6 +3,7 @@ class GenomeModelController < ApplicationController
     @genome_model = Genome::GenomeModel.find(params[:id])
     @builds = @genome_model.builds.sorted_by_date_scheduled
     @last_complete_build = @builds.select {|b| b.master_event.event_status == 'Succeeded'}.first
+    @inputs_by_name = @genome_model.inputs_by_name
 
     respond_to do |format|
       format.html
