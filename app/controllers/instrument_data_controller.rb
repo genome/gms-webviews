5489c5cb490985a9b163086f516a75d672f5a894
Adam Coffman <acoffman@genome.wustl.edu>
2012-04-26 16:02:03 -0500

    instrument data page now shows taxon even if the relation is through an individual or population group

--- app/controllers/instrument_data_controller.rb
+++ app/controllers/instrument_data_controller.rb
@@ -4,7 +4,10 @@ class InstrumentDataController < ApplicationController
     @instrument_data = Genome::InstrumentData.find(params[:id])
     @attributes = @instrument_data.instrument_data_attributes
     @library = @instrument_data.library
+    #taxon could be on the library's sample, or one level higher
+    #if there is a population group or individual record
     @taxon = @library.sample.taxon
+    @taxon = @library.sample.subject.taxon unless @taxon
   end
 
 end
--- app/models/genome/subject.rb
+++ app/models/genome/subject.rb
@@ -10,4 +10,22 @@ class Genome::Subject < ActiveRecord::Base
   scope :with_models_and_builds, includes(models: [{builds: :master_event}, :model_inputs])
   scope :with_attributes, includes(:subject_attributes)
   scope :with_libraries, includes(:libraries)
+
+  def taxon
+    find_subject_for_attribute_name "taxon_id"
+  end
+
+  def subject
+    find_subject_for_attribute_name "source_id"
+  end
+
+  private
+  def find_subject_for_attribute_name(subject_label)
+    subject = self.subject_attributes.select{|a| a.attribute_label == subject_label}.first
+    if subject
+      Genome::Subject.find(subject.attribute_value)
+    else
+      nil
+    end
+  end
 end
