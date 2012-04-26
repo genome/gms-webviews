dcdbfa8820ed7ab96efeb7cb78214f31547b8f88
Adam Coffman <acoffman@genome.wustl.edu>
2012-04-26 16:23:09 -0500

    eager load more stuff for the instrument data page

--- app/controllers/instrument_data_controller.rb
+++ app/controllers/instrument_data_controller.rb
@@ -1,7 +1,7 @@
 class InstrumentDataController < ApplicationController
 
   def show
-    @instrument_data = Genome::InstrumentData.find(params[:id])
+    @instrument_data = Genome::InstrumentData.find_for_instrument_data_page(params[:id])
     @attributes = @instrument_data.instrument_data_attributes
     @library = @instrument_data.library
     #taxon could be on the library's sample, or one level higher
