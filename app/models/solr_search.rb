66d3991cc25ecb912c4303ca09c03e6fa9effa0d
Adam Coffman <acoffman@genome.wustl.edu>
2012-10-22 10:18:29 -0500

    clean up solr search class a little

--- app/models/solr_search.rb
+++ app/models/solr_search.rb
@@ -1,36 +1,30 @@
 class SolrSearch
 
-  class << self
-    def search(opts)
-      opts = {
-        "page" => 1,
-        "page_size" => 20,
-        "facet" => true,
-        "hl" => true,
-        "qs" => 1,
-        "facet.field" => "{!ex=tt}type"
-      }.merge(opts) { |key,old,new| new ? new : old }
+  def self.search(opts)
+    opts = {
+      "page" => 1,
+      "page_size" => 20,
+      "facet" => true,
+      "hl" => true,
+      "qs" => 1,
+      "facet.field" => "{!ex=tt}type"
+    }.merge(opts) { |key,old,new| new ? new : old }
 
-      opts["fq"] = "{!tag=tt}type:\"#{opts["fq"]}\"" if opts["fq"]
+    opts["fq"] = "{!tag=tt}type:\"#{opts["fq"]}\"" if opts["fq"]
 
-      cache_key = "search-#{opts.to_s}"
+    cache_key = "search-#{opts.to_s}"
 
-      if Rails.cache.exist?(cache_key)
-        SolrSearch.new Rails.cache.fetch(cache_key)
-      else
-        solr = RSolr.connect url: SOLR_CONNECTION_STRING
-        results = solr.paginate opts.delete("page"), opts.delete("page_size"), "select", params: opts
+    if Rails.cache.exist?(cache_key)
+      SolrSearch.new Rails.cache.fetch(cache_key)
+    else
+      solr = RSolr.connect url: SOLR_CONNECTION_STRING
+      results = solr.paginate opts.delete("page"), opts.delete("page_size"), "select", params: opts
 
-        unless results["response"]["numFound"] == 0
-          Rails.cache.write(cache_key, results, expires_in: 30.minutes)
-        end
-        SolrSearch.new results
+      unless results["response"]["numFound"] == 0
+        Rails.cache.write(cache_key, results, expires_in: 30.minutes)
       end
+      SolrSearch.new results
     end
-  end
-
-  def initialize(results)
-    @results = results
   end
 
   def facets
@@ -45,5 +39,10 @@ class SolrSearch
     @results["response"]["docs"]
   end
 
+  private
+
+    def initialize(results)
+      @results = results
+    end
 end
 
