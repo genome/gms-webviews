0842ead66f4226ccf4c41ae85885b5ddbf8265fd
Ben Oberkfell <boberkfell@gmail.com>
2012-10-23 13:49:44 -0500

    workflow instance has a plan

--- app/models/genome/workflow_instance.rb
+++ app/models/genome/workflow_instance.rb
@@ -4,4 +4,6 @@ class Genome::WorkflowInstance < ActiveRecord::Base
 
   belongs_to :parent_instance, :class_name=>'Genome::WorkflowInstance'
   has_many :child_instances, :class_name=>'Genome::WorkflowInstance', :foreign_key => :parent_instance_id
+  
+  belongs_to :plan, :class_name=>'Genome::WorkflowPlan', :foreign_key => :workflow_plan_id
 end
