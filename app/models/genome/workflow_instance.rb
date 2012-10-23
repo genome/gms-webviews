3a3fe26dc52cc1fa1e1ad3c9e92e84ce4fe12f50
Ben Oberkfell <boberkfell@gmail.com>
2012-10-23 11:47:33 -0500

    workflow instances have children

--- app/models/genome/workflow_instance.rb
+++ app/models/genome/workflow_instance.rb
@@ -1,3 +1,7 @@
-class Genome::WorkflowInstance
+class Genome::WorkflowInstance < ActiveRecord::Base
   self.table_name = 'workflow.instance'
+  self.primary_key = :workflow_instance_id
+
+  belongs_to :parent_instance, :class_name=>'Genome::WorkflowInstance'
+  has_many :child_instances, :class_name=>'Genome::WorkflowInstance', :foreign_key => :parent_instance_id
 end
