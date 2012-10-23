b4ded18ec444337fbbb15a4e03d226c414abb7ac
Adam Coffman <acoffman@genome.wustl.edu>
2012-10-23 14:33:06 -0500

    workflow instances

--- app/models/genome/workflow_instance.rb
+++ app/models/genome/workflow_instance.rb
@@ -2,8 +2,8 @@ class Genome::WorkflowInstance < ActiveRecord::Base
   self.table_name = 'workflow.instance'
   self.primary_key = :workflow_instance_id
 
-  belongs_to :parent_instance, :class_name=>'Genome::WorkflowInstance'
-  has_many :child_instances, :class_name=>'Genome::WorkflowInstance', :foreign_key => :parent_instance_id
+  belongs_to :parent, :class_name=>'Genome::WorkflowInstance'
+  has_many :children, :class_name=>'Genome::WorkflowInstance', :foreign_key => :parent_instance_id
   
   belongs_to :plan, :class_name=>'Genome::WorkflowPlan', :foreign_key => :workflow_plan_id
   has_one :execution, :class_name=>'Genome::WorkflowInstanceExecution', :foreign_key => :workflow_instance_id
