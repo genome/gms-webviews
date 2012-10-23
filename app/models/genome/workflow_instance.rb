6ab881c893908cc489822060d3d2333c91e16853
Adam Coffman <acoffman@genome.wustl.edu>
2012-10-23 16:35:13 -0500

    whitespace

--- app/models/genome/workflow_instance.rb
+++ app/models/genome/workflow_instance.rb
@@ -4,7 +4,6 @@ class Genome::WorkflowInstance < ActiveRecord::Base
 
   belongs_to :parent, :class_name=>'Genome::WorkflowInstance'
   has_many :children, :class_name=>'Genome::WorkflowInstance', :foreign_key => :parent_instance_id
-  
   belongs_to :plan, :class_name=>'Genome::WorkflowPlan', :foreign_key => :workflow_plan_id
   has_one :execution, :class_name=>'Genome::WorkflowInstanceExecution', :foreign_key => :workflow_instance_id
 end
