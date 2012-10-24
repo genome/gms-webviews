e26a4c538df5ebb6a63c1dcb50cb21e0a2cdc516
Ben Oberkfell <ben@oberkfell.com>
2012-10-24 14:41:24 -0500

    this is a little heinous but it properly drills into workflows now.

--- app/models/genome/workflow_instance.rb
+++ app/models/genome/workflow_instance.rb
@@ -5,12 +5,33 @@ class Genome::WorkflowInstance < ActiveRecord::Base
   has_one    :execution, :class_name=>'Genome::WorkflowInstanceExecution', :foreign_key => :workflow_instance_id
   has_many   :child_operations, :class_name=>'Genome::WorkflowInstance', :foreign_key => :parent_instance_id
   has_many   :child_instances, :class_name=>'Genome::WorkflowInstance', :foreign_key => :parent_execution_id, :primary_key => :current_execution_id
+  has_many   :peers, :class_name=>'Genome::WorkflowInstance', :foreign_key => :peer_instance_id
   belongs_to :plan, :class_name=>'Genome::WorkflowPlan', :foreign_key => :workflow_plan_id
   belongs_to :parent, :class_name=>'Genome::WorkflowInstance'
 
   delegate :status, :start_time, :end_time, :dispatch_id, :stdout, :stderr,  to: :execution
-
+    
   def elapsed_time
     end_time - start_time
+  end
+  
+  def self.tree_for(instance)
+      where("#{table_name}.#{primary_key} IN (#{tree_sql_for(instance)})").order("#{table_name}.#{primary_key}")
+  end
+
+  def self.tree_sql_for(instance)
+    tree_sql =  <<-SQL
+      WITH RECURSIVE search_tree(#{primary_key}, path) AS (
+          SELECT #{primary_key}, ARRAY[]::character varying[]
+          FROM #{table_name}
+          WHERE #{primary_key} = '#{instance.id}'
+        UNION ALL
+          SELECT #{table_name}.#{primary_key}, path || #{table_name}.#{primary_key}
+          FROM search_tree
+          JOIN #{table_name} ON #{table_name}.parent_instance_id = search_tree.#{primary_key} 
+          WHERE NOT #{table_name}.#{primary_key} = ANY(path)
+      )
+      SELECT #{primary_key} FROM search_tree ORDER BY path
+    SQL
   end
 end
--- app/models/genome/workflow_plan.rb
+++ app/models/genome/workflow_plan.rb
@@ -4,6 +4,7 @@ class Genome::WorkflowPlan < ActiveRecord::Base
 
   def operations
     parsed_plan = Nokogiri::XML( self.xml )
+    
     parse_operations_for_plan( parsed_plan )
   end
 
@@ -20,23 +21,37 @@ class Genome::WorkflowPlan < ActiveRecord::Base
     end
 
     def workflow_only_operations(parsed_plan)
-      workflow_name = parsed_plan.xpath("/workflow").attr("name").to_s
-      parent_instance = Genome::WorkflowInstance.find_by_name(workflow_name)
+      parent_instance = Genome::WorkflowInstance.where(parent_instance_id: nil, workflow_plan_id: self.id).first
+      if parent_instance.peer_instance_id
+        parent_instance = Genome::WorkflowInstance.find(parent_instance.peer_instance_id)
+      end
 
       op_names = []
-      operations = parsed_plan.xpath("//operation[@name != 'merge results']")
+      operations = parsed_plan.xpath("//operation[operationtype/@commandClass]")
 
       operations.each do |op|
         op_names.push op.attr("name")
       end
+      
+      by_names = Genome::WorkflowInstance.tree_for(parent_instance  ).inject(Hash.new {|h,k| h[k] = []}) do |h, i|
+        h.tap do |h|
+          h[i.name].concat [i]
+        end
+      end
 
-      by_names = Genome::WorkflowInstance.where(name: op_names, parent_instance_id: parent_instance.id).inject({}) {|h,i| h.tap{|h| h[i.name] = i}}
-      return op_names.collect {|name| by_names[name]}
+      out = []
+      
+      op_names.each do |name|
+        out.concat by_names[name]
+        out.concat by_names[name].flat_map {|op| op.peers.all}
+      end
+
+      return out.uniq_by {|obj| obj.id}
     end
 
     def parse_operations_for_plan( parsed_plan )
-      workflow_name = parsed_plan.xpath("/workflow").attr("name").to_s
-      if workflow_name =~ /all stages$/
+      workflow_node = parsed_plan.xpath("/workflow")
+      if workflow_node && workflow_node.attr("name").to_s =~ /all stages$/
         return legacy_staged_operations(parsed_plan)
       else
         return workflow_only_operations(parsed_plan)
