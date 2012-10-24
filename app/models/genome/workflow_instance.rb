6a8fb17942e9bc70e014ca1a558537425ba537c7
Adam Coffman <acoffman@genome.wustl.edu>
2012-10-24 10:38:03 -0500

    both relations on workflow instace

--- app/models/genome/workflow_instance.rb
+++ app/models/genome/workflow_instance.rb
@@ -3,7 +3,8 @@ class Genome::WorkflowInstance < ActiveRecord::Base
   self.primary_key = :workflow_instance_id
 
   has_one    :execution, :class_name=>'Genome::WorkflowInstanceExecution', :foreign_key => :workflow_instance_id
-  has_many   :children, :class_name=>'Genome::WorkflowInstance', :foreign_key => :parent_instance_id
+  has_many   :child_operations, :class_name=>'Genome::WorkflowInstance', :foreign_key => :parent_instance_id
+  has_many   :child_instances, :class_name=>'Genome::WorkflowInstance', :foreign_key => :parent_execution_id, :primary_key => :current_execution_id
   belongs_to :plan, :class_name=>'Genome::WorkflowPlan', :foreign_key => :workflow_plan_id
   belongs_to :parent, :class_name=>'Genome::WorkflowInstance'
 
