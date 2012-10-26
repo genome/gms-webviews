933275294ead165ce5cfc730fbab79b01dd2e675
Adam Coffman <acoffman@genome.wustl.edu>
2012-10-26 14:10:35 -0500

    workflow, maybe

--- app/presenters/build_presenter.rb
+++ app/presenters/build_presenter.rb
@@ -41,7 +41,7 @@ class BuildPresenter < SimpleDelegator
       "files"              => ->(b) { link_span('data', data_dir(b)) + link_span('errors', log_dir(b,'err')) + link_span('output', log_dir(b,'out')) },
       "software"           => 'stuff',
       "processing_profile" => ->(b) { link_span(b.model.processing_profile.name, processing_profile_path(b.model.processing_profile))},
-      "workflow"           => "#{@build.workflow_instance.name} (#{@build.workflow_instance.id})",
+      "workflow"           => "#{Maybe(@build.workflow_instance).name} (#{Maybe(@build.workflow_instance).id})",
       "LSF job id"         => 'stuff'
     }
   end
