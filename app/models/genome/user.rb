e778fec78b9320ccf88e71afeb6383462eccf9aa
Ben Oberkfell <boberkfe@genome.wustl.edu>
2012-10-29 16:38:01 -0500

    get watched/created projects for a user

--- app/models/genome/user.rb
+++ app/models/genome/user.rb
@@ -1,3 +1,17 @@
 class Genome::User < ActiveRecord::Base
   self.table_name = 'subject.user'
+
+  def created_projects 
+    get_project_by_role('creator')
+  end
+  def watched_projects 
+    get_project_by_role('watcher')
+  end
+
+
+  private
+
+  def get_project_by_role(role)
+    Genome::Project.joins(:project_parts).where(project_part: {part_id: email, role: role})
+  end
 end
