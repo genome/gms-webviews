b5e5b45c84f1d018c41f2f910fe7d0698475438d
Ben Oberkfell <boberkfell@gmail.com>
2012-10-30 14:19:15 -0500

    genericise a datatable class

---
+++ app/datatables/datatable.rb
@@ -0,0 +1,51 @@
+class Datatable
+  
+  def model_class
+    "DefineThisInYourSubclassToStopThisFromCrashing"
+  end
+  
+  def initialize(view)
+    @view = view
+  end
+  
+  def as_json(options={})
+    {
+      sEcho: params[:sEcho].to_i,
+      iTotalRecords: model_class_object.count,
+      iTotalDisplayRecords: objects.total_entries,
+      aaData:data
+    }
+  end  
+  
+  def objects
+    @objects ||= fetch_objects
+  end
+  
+  def fetch_objects
+    objects = model_class_object.order("#{sort_column} #{sort_direction}").includes(:processing_profile)
+    objects = objects.page(page).per_page(per_page)
+    if params[:sSearch].present?
+      objects = objects.where("name like :search", search: "%#{params[:sSearch]}%")
+    end
+    
+    objects
+  end
+  
+  
+  def page
+      params[:iDisplayStart].to_i/per_page + 1
+  end
+
+  def per_page
+    params[:iDisplayLength].to_i > 0 ? params[:iDisplayLength].to_i : 10
+  end
+  
+  def sort_direction
+    params[:sSortDir_0] == "desc" ? "desc" : "asc"
+  end  
+  
+  private
+    def model_class_object
+      self.model_class.constantize
+    end
+  end
