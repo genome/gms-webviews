348a2abf8de0371c5e0fadf2657737d250a5c11b
Ben Oberkfell <boberkfell@gmail.com>
2012-10-30 14:32:27 -0500

    add processing profile datatable

--- app/controllers/processing_profile_controller.rb
+++ app/controllers/processing_profile_controller.rb
@@ -1,4 +1,12 @@
 class ProcessingProfileController < ApplicationController
+  def index
+    respond_to do |format|
+      format.html
+      format.json { render json: ProcessingProfilesDatatable.new(view_context) }
+    end
+  end
+  
+  
   def show
     processing_profile =  Genome::ProcessingProfile.find(params[:id], include: :processing_profile_params)
     @processing_profile = ProcessingProfilePresenter.new(processing_profile)
