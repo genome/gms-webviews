67d8ebeb031eade638277d2c0eb5101a58f7c1b5
Adam Coffman <acoffman@genome.wustl.edu>
2012-10-30 13:03:22 -0500

    whitespace

--- app/models/genome/user.rb
+++ app/models/genome/user.rb
@@ -1,17 +1,16 @@
 class Genome::User < ActiveRecord::Base
   self.table_name = 'subject.user'
 
-  def created_projects 
-    get_project_by_role('creator')
+  def created_projects
+    get_project_by_role( 'creator' )
   end
-  def watched_projects 
-    get_project_by_role('watcher')
+  def watched_projects
+    get_project_by_role( 'watcher' )
   end
 
 
   private
-
-  def get_project_by_role(role)
-    Genome::Project.joins(:project_parts).where(project_part: {part_id: email, role: role})
-  end
+    def get_project_by_role( role )
+      Genome::Project.joins( :project_parts ).where( project_part: { part_id: email, role: role } )
+    end
 end
