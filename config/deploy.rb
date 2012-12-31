c57b1ddcad7dceaa4cb0bb7333ec69a2cdfeb2b6
Ben Oberkfell <boberkfell@gmail.com>
2012-12-31 16:19:48 -0600

    stub in a capistrano control file

---
+++ config/deploy.rb
@@ -0,0 +1,48 @@
+# swiped this capfile from this doc:
+# http://kris.me.uk/2011/10/28/rails-rvm-passenger-capistrano-git-apache.html
+
+# RVM bootstrap
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+require 'rvm/capistrano'
+set :rvm_ruby_string, '1.9.3-p362'
+
+# bundler bootstrap
+require 'bundler/capistrano'
+
+# main details
+set :application, "vmpool85"
+role :web, "vmpool85"
+role :app, "vmpool85"
+role :db,  "vmpool85", :primary => true
+
+# server details
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+set :deploy_to, "/var/www/gms-webviews"
+set :deploy_via, :remote_cache
+set :user, "passenger"
+ssh_options[:keys] = [File.join(ENV["HOME"], "ssh-keys", "passenger@rails-webviews")]
+set :use_sudo, false
+
+# repo details
+set :scm, :git
+#set :scm_username, "passenger"
+set :repository, "https://github.com/genome/gms-webviews.git"
+set :branch, "master"
+set :git_enable_submodules, 1
+
+# tasks
+namespace :deploy do
+  task :start, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  task :stop, :roles => :app do
+    # Do nothing.
+  end
+
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
