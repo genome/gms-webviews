8cc3e22cf2e167dad3a828038f8882c83975ce78
Adam Coffman <acoffman@genome.wustl.edu>
2013-01-22 15:57:23 -0600

    switch to clearer each_with_object

--- app/presenters/processing_profile_presenter.rb
+++ app/presenters/processing_profile_presenter.rb
@@ -12,14 +12,12 @@ class ProcessingProfilePresenter < SimpleDelegator
   end
 
   def attribute_pairs
-    @pairs ||= @processing_profile.processing_profile_params.inject({}) do |attribute_list, param|
-      attribute_list.tap do |al|
-        al[param.param_name] = ->(b) do
-          if param.param_name == 'reference_sequence_model_id'
-            link_span(param.param_value, model_path(param.param_value))
-          else
-            param.param_value
-          end
+    @pairs ||= processing_profile_params.each_with_object({}) do |param, h|
+      h[param.param_name] = ->(p) do
+        if param.param_name == 'reference_sequence_model_id'
+          link_span(param.param_value, model_path(param.param_value))
+        else
+          param.param_value
         end
       end
     end
