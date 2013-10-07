859257bb65113b1ab4fe94714ccb7d2dbf7b3315
Adam Coffman <acoffman@genome.wustl.edu>
2013-10-07 13:37:25 -0500

    remove attribute scope for now

--- app/controllers/taxon_controller.rb
+++ app/controllers/taxon_controller.rb
@@ -1,6 +1,6 @@
 class TaxonController < ApplicationController
   def show
-    subject = Genome::Subject.where(subject_id: params[:id]).with_attributes.first
+    subject = Genome::Subject.where(subject_id: params[:id]).first
     @subject = SubjectPresenter.new(subject)
     render 'subject/show'
   end
