8b1084705f22f9a35e79880acb3b70155a6d560f
Adam Coffman <acoffman@genome.wustl.edu>
2014-01-27 15:03:29 -0600

    read gms_id from /etc

--- config/initializers/gsc_path.rb
+++ config/initializers/gsc_path.rb
@@ -3,3 +3,7 @@ APP_CONFIG ||= {}
 APP_CONFIG.merge!(YAML.load_file(Rails.root.join('config', 'gsc.yml'))[Rails.env])
 
 GSC_WEB_PATH =  APP_CONFIG['gsc']['web_path']
+
+gms_id = `cat /etc/genome/sysid`
+
+ENV['GMS_ID'] = gms_id.strip
